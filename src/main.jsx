7c3cb3f84e77aa79d4924fe390218dff8ba239e9
mishael-codes <enyimishael22@gmail.com>
2025-06-11 17:21:30 +0100

    Fix: bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/stocks" element={<Stocks />} />
-        <Route path="/stocks/:symbol" element={<StockDetail />} />
+        {/* <Route path="/stocks/:symbol" element={<StockDetail />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
